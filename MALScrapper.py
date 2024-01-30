c08ad842e39574c59220fe3d841e531f4ef579f3
ravaen3 <theravaen@gmail.com>
2024-01-30 20:09:15 +0100

    First Commit

---
+++ MALScrapper.py
@@ -0,0 +1,55 @@
+from os import name
+from pyparsing import Char
+import re
+import requests
+import time
+import jsonpickle
+import random
+from sqids import Sqids
+from bs4 import BeautifulSoup
+character_amount = 5000
+series_whitelist = [] #"https://myanimelist.net/anime/8234/Muumin"
+series_names = ["One Piece", "Dragon Ball"]
+amount_scraped = 0
+class Character:
+    def __init__(self, name, series, img_url ="test"):
+        self.name = name
+        self.series = series
+        self.img_url = img_url
+
+characters = {}
+for i in range(0,character_amount//50):
+    URL = ("https://myanimelist.net/character.php?limit="+str(i*50))
+    html = BeautifulSoup(requests.get(URL).text)
+    for entry in html.find_all("tr", attrs={"class":"ranking-list"}):
+        name = re.findall("(?<=\d\/)(.*?)(?=\")",str(entry.find("a", attrs={"class":"fs14 fw-b"})))[0]
+        series = ""
+        series_arr = entry.find("td", attrs={"class":"animeography"}).find_all("a")
+        series_arr.extend(entry.find("td", attrs={"class":"mangaography"}).find_all("a"))
+        for anime in series_arr:
+            if anime.get_text() in series_names:
+                series = anime.get_text()
+                break
+            if len(anime.get_text())<len(series) or series == "":
+                series = anime.get_text() 
+        image_id = re.findall("(?<=rs\/)(.*?)\?", str(entry.find("img", attrs={"class":"lazyload"})))
+        image_url = "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+        if len(image_id) != 0:
+            image_url = "https://cdn.myanimelist.net/images/characters/" +image_id[0]
+       
+        character = Character(
+            re.findall("(?<=\d\/)(.*?)(?=\")",str(entry.find("a", attrs={"class":"fs14 fw-b"})))[0],
+            str(series),
+            image_url
+        )
+        characters[name]=character
+        print(amount_scraped)
+        amount_scraped+=1
+    time.sleep(random.randint(3,6))
+for URL in series_whitelist:
+    html = BeautifulSoup(requests.get(URL+"/characters").text)
+    for entry in html.find("div", attrs={"class":"rightside js-scrollfix-bottom-rel"}).find_all("table", attrs={"class":"js-anime-character-table"}):
+        print(entry.find("h3"))
+f = open("Characters/data.json", "w")
+f.write(jsonpickle.encode(characters))
+f.close()
---
+++ bot.py
@@ -0,0 +1,68 @@
+from asyncio.windows_events import NULL
+import random
+from tracemalloc import start
+import jsonpickle
+import discord
+from discord.ext import commands
+import time
+import os
+from sqids import Sqids
+
+
+
+sqids = Sqids(alphabet="kEjqW4T673ePsJNoACFwUVy1Ofabmz5nxGDtcHZQ2lpgrSLdhM0uR8iKIvBX9Y", min_length=6)
+
+class BotData(object):
+    pass
+class Character:
+    def __init__(self, name, series, img_url ="test"):
+        self.name = name
+        self.series = series
+        self.img_url = img_url
+
+class Card:
+    def __init__(self, id, character):
+        self.id = sqids.encode(id)
+        self.character = character
+class Player:
+    def __init__(self, ):
+        pass
+    
+def load_data():
+    f = open("data.json")
+    botdata = jsonpickle.decode(f.read())
+    f.close()
+    print(botdata.current_id)
+
+def load_characters():
+    f = open("Characters\data.json", "r")
+    data = jsonpickle.decode(f.read())
+    f.close()
+    return data
+def generate_card_drop():
+    random.seed(time.time())
+    drop = random.choice(list(characters.values()))
+    return drop
+def load_card():
+    pass
+characters = load_characters()
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!",intents=intents)
+@bot.event
+async def on_ready():
+    print("We have logged in as {bot.user}")
+@bot.command(aliases=["r"])
+async def register(ctx):
+    sender_id = ctx.author.id
+    is_registered = os.path.exists("Players/"+sender_id)
+    if is_registered:
+        print("You are already registered!")
+    else:
+        f = open("Players/"+str(sender_id), "w")
+        f.write()
+        
+bot.run('MTAwNjkwODA3MjQ5NDYzNzA3Ng.G3D-72.cJBtxEnMHni9K8LkgoKtHO0BkzSMBDMTJIlmZQ')
+load_data()
+print(generate_card_drop())
