87b9bde22bfedd007006785432d5f70c61deaaaa
DoodleyJC <dijkstradesley@gmail.com>
2024-01-31 19:30:53 +0100

    improved example

--- randomId/example_randomId.py
+++ randomId/example_randomId.py
@@ -1,4 +1,12 @@
 import randomIdPrototype
 
-aux =randomIdPrototype.reverseCharId(randomIdPrototype.reverseCharString(input("what you wanna search")))
-print(aux, "maps to", randomIdPrototype.getCharString(randomIdPrototype.getCharId(aux)))
+inputVar = input("what u wanna search\n type \"a\" for string to id\n type \"b\" for id to string\n")
+if(inputVar=="a"): 
+    aux =randomIdPrototype.reverseCharId(randomIdPrototype.reverseCharString(input("what you wanna search")))
+    print(aux, "maps to", randomIdPrototype.getCharString(randomIdPrototype.getCharId(aux)))
+elif inputVar=="b":
+    number = int(input("type a number"))
+    aux = randomIdPrototype.getCharString(randomIdPrototype.getCharId(number))
+    print(number, "maps to", aux)
+else: 
+    print("type properly idiot")
